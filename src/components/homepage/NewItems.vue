d7b2ed364e30afa4655411e074a512876541e20a
Ahmad Mushawwir <mushawwirahmad45@gmail.com>
2024-03-01 17:11:51 +0700

    created UI Component

---
+++ src/components/homepage/NewItems.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from "vue";
+import NewItemsCard from "../NewItemsCard.vue";
+
+const newItems = ref([
+  {
+    id: 1,
+    title: "Mobile UI Kit",
+    image: "items-1.jpg",
+    subtitle: "Mobile UI Kit",
+  },
+  {
+    id: 2,
+    title: "Online Doctor Consultation",
+    image: "items-2.jpg",
+    subtitle: "Mobile UI Kit",
+  },
+  {
+    id: 3,
+    title: "Banking Crypto",
+    image: "items-3.jpg",
+    subtitle: "Mobile UI Kit",
+  },
+]);
+</script>
+
+<template>
+  <div class="container px-4 mx-auto my-16 md:px-12">
+    <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
+    <div class="flex flex-wrap -mx-1 lg:-mx-4">
+      <NewItemsCard
+        v-for="newItem in newItems"
+        :key="newItem.id"
+        :title="newItem.title"
+        :image="newItem.image"
+        :subtitle="newItem.subtitle"
+      />
+    </div>
+  </div>
+</template>
---
+++ src/components/layouts/Logo.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-link to="/" class="flex items-center">
+      <img
+        src="@/assets/img/logo.svg"
+        class="h-8 ml-3 sm:ml-0 sm:h-8"
+        alt="Zullkit Logo"
+      />
+  </router-link>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
